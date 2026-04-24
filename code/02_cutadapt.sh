16347a0efa9d4f57e6191143a36848ec5cfdda36
Evan J. Christensen <evan.christensen@unh.edu>
2026-04-24 16:07:59 -0400

    Clean up cutadapt.sh by removing project-specific details
    Removed project-specific comments and parameters from the cutadapt script.

--- code/02_cutadapt.sh
+++ code/02_cutadapt.sh
@@ -3,26 +3,6 @@
 #!/bin/bash
 source ~/Metabarcoding-of-Algae/code/qiime2_parameters.sh
 
-### THIS IS WHERE PROJECTS BEGIN TO DIVERGE. The cutadapt parameters and primers will depend on the project. See qiime2_parameters.sh for cutadapt parameters and 01_trim.sh for polyG filter parameters.
-primer="RBCL"
-projname="DEP_${primer}"
-## example: projname="Cyanobac_16s_V4-V5"
-
-conda activate qiime2-amplicon-2026.1
-
-### import fastqs. Add the demultiplexed sequences to the data/results directory. This will create a .qza file that can be used for cutadapt and qiime2 downstream analyses.
-qiime tools import \
-    --type "SampleData[PairedEndSequencesWithQuality]"  \
-    --input-format CasavaOneEightSingleLanePerSampleDirFmt \
-    --input-path data/poly-G-trimmed \
-    --output-path data/results/${projname}_demux 
-
-
-## copied from qiime2_parameters.sh
-fw='^GTGYCAGCMGCCGCGGTAA'
-rv='^CCGYCAATTYMTTTRAGTTT'
-cutadapt_config="--p-front-f $fw --p-front-r $rv"
-
 ### See qiime2_parameters.sh for cutadapt parameters and 01_trim.sh for polyG filter parameters.
 qiime cutadapt trim-paired \
     --i-demultiplexed-sequences data/results/${projname}_demux.qza \
